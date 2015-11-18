da96a11b1e7a5ce60c604deb2d0d54c1c9ef2e20
ehnalis <zz@somewhat.com>
2015-11-18 00:54:10 +0000

    Pagerank Mapper.

--- src/main/java/hu/sztaki/workshop/hadoop/d02/pagerank/Mapper.java
+++ src/main/java/hu/sztaki/workshop/hadoop/d02/pagerank/Mapper.java
@@ -1,6 +1,11 @@
 package hu.sztaki.workshop.hadoop.d02.pagerank;
 
+import hu.sztaki.workshop.hadoop.d02.Pagerank;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 /**
  * @todo Finish class.
@@ -20,6 +25,36 @@ public class Mapper extends org.apache.hadoop.mapreduce.Mapper<Text, Text, Text,
      * @hint Set `d`, the dumping factor to 0.85.
      */
     public void map(Text key, Text value, Context context){
-        
+        StringTokenizer st = new StringTokenizer(value.toString(), Pagerank.separator);
+        int degree = st.countTokens();
+        Double rank;
+
+        int current_round =
+                context.getConfiguration()
+                        .getInt("hu.sztaki.workshop.spark.d02.pagerank.current_iteration", 0);
+
+        if(current_round == 0) {
+            rank = (1.0/degree) * d * (1.0/n) + (1-d) * (1.0/n);
+        } else {
+            double currentRank = Double.parseDouble(st.nextToken());
+            rank = (1.0/degree) * d * currentRank + (1-d) * (1.0/n);
+        }
+
+        Rank rankValue = new Rank(new DoubleWritable(rank));
+        Rank outlinkValue;
+
+        while(st.hasMoreTokens()) {
+            String outlink = st.nextToken();
+            outlinkValue = new Rank(new Text(outlink));
+
+            try {
+                context.write(new Text(outlink), rankValue);
+                context.write(key, outlinkValue);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
