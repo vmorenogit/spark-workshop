9bc9f0513cfa5ba6027da9a02f45c2a6cd494118
zoltan.zvara <zoltan.zvara@gmail.com>
2015-11-19 06:30:41 -0600

    Bigram fixed.

--- src/main/scala-2.11/hu/sztaki/workshop/spark/d03/e3/Bigram.scala
+++ src/main/scala-2.11/hu/sztaki/workshop/spark/d03/e3/Bigram.scala
@@ -22,10 +22,5 @@ object Bigram {
   /**
     * @todo Complete function.
     */
-  def apply(input: String): List[Bigram] = {
-    val splitWords = input.split(" ")
-    if(splitWords.size < 2) return List[Bigram]()
-    val slidingWords = splitWords.sliding(2).toList
-    slidingWords.map(words => new Bigram(words(0), words(1)))
-  }
+  def apply(input: String): List[Bigram] = ???
 }
