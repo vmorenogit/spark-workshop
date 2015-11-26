81f985b391b4a9218f5c27720b358e6d74e88419
ehnalis <zoltan.zvara@gmail.com>
2015-11-26 17:22:11 +0100

    Fixed

--- src/main/scala-2.11/hu/sztaki/workshop/spark/d09/e01/StreamingLogInput.scala
+++ src/main/scala-2.11/hu/sztaki/workshop/spark/d09/e01/StreamingLogInput.scala
@@ -10,36 +10,46 @@ object StreamingLogInput {
     /**
       * @todo[1] Create SparkConf.
       */
-
+    val conf = new SparkConf()
+      .setAppName("Bla bla error app")
+      .setMaster("local")
     /**
       * @todo[2] Create StreamingContext with a batch duration of 1 second.
       */
+    val ssc = new StreamingContext(conf, Seconds(5))
 
     /**
       * @todo[2] Create a DStream from all the input on port 7777.
       * @hint You need to create a socket listener.
       */
+    val input = ssc.socketTextStream("localhost", 7777)
+    val errorLines = processLines(input)
 
     /**
       * @todo[4] Print out the DStream.
       */
+    errorLines.print()
 
     /**
       * @todo[5] Start the streaming context and wait for it to finish.
       */
+    ssc.start()
 
     /**
       * @todo[6] Wait for termination: 60 seconds.
       */
+    ssc.awaitTermination(60000)
 
     /**
       * @todo[7] Stop streaming context.
       */
+    ssc.stop()
 
   }
   def processLines(lines: DStream[String]) = {
     /**
       * @todo[3] Filter our DStream for lines with "error".
       */
+    lines.filter(_.contains("error"))
   }
 }
--- src/main/scala-2.11/hu/sztaki/workshop/spark/d09/e01/Wordcount.scala
+++ src/main/scala-2.11/hu/sztaki/workshop/spark/d09/e01/Wordcount.scala
@@ -5,7 +5,7 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object Wordcount {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setAppName("Streaming wordcount")
+    val conf = new SparkConf().setAppName("Streaming wordcount").setMaster("local")
     val ssc = new StreamingContext(conf, Seconds(1))
     val lines = ssc.socketTextStream("localhost", 9999)
 
